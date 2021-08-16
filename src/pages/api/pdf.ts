222d35f5d331c4ae61edd2dd918f10de1b1599f2
andy chou <andy770921@yahoo.com.tw>
2021-08-16 16:04:13 +0800

    env: use chrome-aws-lambda for serverless function

--- src/pages/api/pdf.ts
+++ src/pages/api/pdf.ts
@@ -1,5 +1,57 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import pdf from 'html-pdf-node-ts';
+import { PDFOptions } from 'puppeteer';
+import { Promise as PromiseBluebird } from 'bluebird';
+import hb from 'handlebars';
+import chromium from 'chrome-aws-lambda';
+
+type CallBackType = (pdf: any) => void;
+
+interface OptionsProps extends PDFOptions {
+  args?: string[];
+}
+
+interface FileWithUrl {
+  url: string;
+  content?: never;
+}
+
+interface FileWithContent {
+  url?: never;
+  content: string;
+}
+
+type FileType = FileWithUrl | FileWithContent;
+
+async function generatePdf(file: FileType, options?: OptionsProps, callback?: CallBackType) {
+  const browser = await chromium.puppeteer.launch({
+    args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: true,
+    ignoreHTTPSErrors: true,
+  });
+
+  const page = await browser.newPage();
+
+  if (file.content) {
+    const template = hb.compile(file.content, { strict: true });
+    const result = template(file.content);
+    const html = result;
+
+    await page.setContent(html);
+  } else {
+    await page.goto(file.url as string, {
+      waitUntil: 'networkidle0',
+    });
+  }
+
+  return PromiseBluebird.props(page.pdf(options))
+    .then(async (data) => {
+      await browser.close();
+
+      return Buffer.from(Object.values(data));
+    }).asCallback(callback);
+}
 
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
   const html = `
@@ -196,7 +248,7 @@ export default function handler(_req: NextApiRequest, res: NextApiResponse) {
 </html>
 `;
 
-  pdf.generatePdf({ content: html }, { format: 'a4' }).then((pdfBuffer) => {
+  generatePdf({ content: html }, { format: 'a4' }).then((pdfBuffer) => {
     res.setHeader('Content-Type', 'application/pdf');
     // res.setHeader('Content-Disposition', 'attachment; filename=quotation.pdf');
     res.status(200).send(pdfBuffer);
